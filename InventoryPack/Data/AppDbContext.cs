90dcf376a3a31f64473bae42a32e41b3c280be56
Pusyok <45900786+Pusyok@users.noreply.github.com>
2026-09-10 19:26:50 +0300

    feat(db): AppDbContext with SQLite provider

---
+++ InventoryPack/Data/AppDbContext.cs
@@ -0,0 +1,41 @@
+using InventoryPack.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryPack.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Asset> Assets => Set<Asset>();
+    public DbSet<RejectedRow> RejectedRows => Set<RejectedRow>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Asset>(entity =>
+        {
+            entity.HasKey(a => a.Id);
+
+            entity.Property(a => a.InventoryNumber).HasMaxLength(32);
+            entity.Property(a => a.Name).HasMaxLength(500);
+            entity.Property(a => a.Mvo).HasMaxLength(150);
+            entity.Property(a => a.Subaccount).HasMaxLength(32);
+
+            entity.HasIndex(a => a.InventoryNumber);
+            entity.HasIndex(a => new { a.Mvo, a.Subaccount });
+            entity.HasIndex(a => a.PrintedAt);
+        });
+
+        modelBuilder.Entity<RejectedRow>(entity =>
+        {
+            entity.HasKey(r => r.Id);
+
+            entity.Property(r => r.RawText).HasMaxLength(2000);
+            entity.Property(r => r.Reason).HasMaxLength(100);
+            entity.Property(r => r.Subaccount).HasMaxLength(32);
+            entity.Property(r => r.Mvo).HasMaxLength(150);
+
+            entity.HasIndex(r => r.RowNumber);
+        });
+    }
+}
